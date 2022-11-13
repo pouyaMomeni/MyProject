5ca6c1e06b7640b2b8908c07f58e2a899e3aa93a
pouyaMomeni <pouyalol44@gmail.com>
2022-11-13 10:41:21 +0330

    i18next josn for fullTable

--- components/table/fullTable.js
+++ components/table/fullTable.js
@@ -3,74 +3,76 @@ import { CgProfile } from 'react-icons/cg'
 import { AiOutlineWarning } from 'react-icons/ai'
 import { RiProfileLine } from 'react-icons/ri'
 import { useFull } from 'hook'
+import { useTranslation } from 'next-i18next';
 export default function Full() {
     const { data } = useFull()
+    const { t } = useTranslation();
     return (
         <Flex flexDir='column' p='10' border='1px'>
             <Flex w='full' justifyContent='center' borderBottom='1px'>
                 <Box display='flex' p='5' justifyContent='center' w='30%'><CgProfile size={90} /></Box>
-                <Text fontWeight='bold' w='70%' display='flex' alignItems='center' gap='2' ><AiOutlineWarning size={25} />You can see your Full Information about your acount</Text>
+                <Text fontWeight='bold' w='70%' display='flex' alignItems='center' gap='2' ><AiOutlineWarning size={25} />{t("common:warn-full")}</Text>
             </Flex>
             <Flex flexDir='column' gap='5' padding='5'>
                 {/* row 1 */}
                 <Flex w='full' gap='5' flexDirection={{ base: 'column', lg: 'row' }}>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>ID :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:id-sum")} :</Text>
                         <Input value={data?.data.Data.Id} w='80%' disabled />
                     </Box>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Username :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:username-sum")} :</Text>
                         <Input value={data?.data.Data.Username} w='80%' disabled />
                     </Box>
                 </Flex>
                 {/* row 2 */}
                 <Flex w='full' gap='5' flexDirection={{ base: 'column', lg: 'row' }}>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Mobile :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:mobile-sum")} :</Text>
                         <Input value={data?.data.Data.MobileNumber} w='80%' disabled />
                     </Box>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>FirstName :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:firstName-sum")} :</Text>
                         <Input value={data?.data.Data.FirstName} w='80%' disabled />
                     </Box>
                 </Flex>
                 {/* row 3 */}
                 <Flex w='full' gap='5' flexDirection={{ base: 'column', lg: 'row' }}>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>LastName :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:lastName-sum")} :</Text>
                         <Input value={data?.data.Data.LastName} w='80%' disabled />
                     </Box>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Father name :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:fatherName-full")} :</Text>
                         <Input value={data?.data.Data.FatherName} w='80%' disabled />
                     </Box>
                 </Flex>
                 {/* row 4 */}
                 <Flex w='full' gap='5' flexDirection={{ base: 'column', lg: 'row' }}>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Gender :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:gender-full")} :</Text>
                         <Input value={data?.data.Data.Gender} w='80%' disabled />
                     </Box>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Birthday :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:birth-full")} :</Text>
                         <Input value={data?.data.Data.BirthDate} w='80%' disabled />
                     </Box>
                 </Flex>
                 {/* row 5 */}
                 <Flex w='full' gap='5' flexDirection={{ base: 'column', lg: 'row' }}>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Address :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:address-full")} :</Text>
                         <Input value={data?.data.Data.Address} w='80%' disabled />
                     </Box>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Email :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:email-sum")} :</Text>
                         <Input value={data?.data.Data.Email} w='80%' disabled />
                     </Box>
                 </Flex>
 
 
             </Flex>
-            <Flex borderTop='1px' p='5' justifyContent='center' mt='5'><Text display='flex' gap='2' fontWeight='bold' fontStyle='italic'><RiProfileLine size={25} />Profile</Text></Flex>
+            <Flex borderTop='1px' p='5' justifyContent='center' mt='5'><Text display='flex' gap='2' fontWeight='bold' fontStyle='italic'><RiProfileLine size={25} />{t("common:profile-sum")}</Text></Flex>
         </Flex>
     )
 }
--- components/table/summeryTable.js
+++ components/table/summeryTable.js
@@ -3,52 +3,54 @@ import { CgProfile } from 'react-icons/cg'
 import { AiOutlineWarning } from 'react-icons/ai'
 import { RiProfileLine } from 'react-icons/ri'
 import { useSummary } from 'hook'
+import { useTranslation } from 'next-i18next';
 export default function Summary() {
     const { data } = useSummary()
+    const { t } = useTranslation();
     return (
         <Flex flexDir='column' p='10' border='1px'>
             <Flex w='full' justifyContent='center' borderBottom='1px'>
                 <Box display='flex' p='5' justifyContent='center' w='30%'><CgProfile size={90} /></Box>
-                <Text fontWeight='bold' w='70%' display='flex' alignItems='center' gap='2' ><AiOutlineWarning size={25} />You can see your brief Information about your acount</Text>
+                <Text fontWeight='bold' w='70%' display='flex' alignItems='center' gap='2' ><AiOutlineWarning size={25} />{t("common:warn-sum")}</Text>
             </Flex>
             <Flex flexDir='column' gap='5' padding='5'>
                 {/* row 1 */}
                 <Flex w='full' gap='5' flexDirection={{ base: 'column', lg: 'row' }}>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>ID :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:id-sum")} :</Text>
                         <Input value={data?.data.Data.UserID} w='80%' disabled />
                     </Box>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Username :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:username-sum")} :</Text>
                         <Input value={data?.data.Data.Username} w='80%' disabled />
                     </Box>
                 </Flex>
                 {/* row 2 */}
                 <Flex w='full' gap='5' flexDirection={{ base: 'column', lg: 'row' }}>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Mobile :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:mobile-sum")} :</Text>
                         <Input value={data?.data.Data.Mobile} w='80%' disabled />
                     </Box>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>FirstName :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:firstName-sum")} :</Text>
                         <Input value={data?.data.Data.FirstName} w='80%' disabled />
                     </Box>
                 </Flex>
                 {/* row 3 */}
                 <Flex w='full' gap='5' flexDirection={{ base: 'column', lg: 'row' }}>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>LastName :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:lastName-sum")} :</Text>
                         <Input value={data?.data.Data.LastName} w='80%' disabled />
                     </Box>
                     <Box display='flex' alignItems='center' gap='2' w={{ base: 'full', lg: '50%' }}>
-                        <Text display='flex' justifyContent='center' w='20%'>Email :</Text>
+                        <Text display='flex' justifyContent='center' w='20%'>{t("common:email-sum")} :</Text>
                         <Input value={data?.data.Data.Email} w='80%' disabled />
                     </Box>
                 </Flex>
 
 
             </Flex>
-            <Flex borderTop='1px' p='5' justifyContent='center' mt='5'><Text display='flex' gap='2' fontWeight='bold' fontStyle='italic'><RiProfileLine size={25} />Profile</Text></Flex>
+            <Flex borderTop='1px' p='5' justifyContent='center' mt='5'><Text display='flex' gap='2' fontWeight='bold' fontStyle='italic'><RiProfileLine size={25} />{t("common:profile-sum")}</Text></Flex>
         </Flex>
     )
 }
